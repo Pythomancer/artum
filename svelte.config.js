3949fe467e80fcde5f2e09719228ea7fcf7b981b
ash <niklaustermann@gmail.com>
2022-12-12 11:52:28 +0100

    rem sverdle

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter(
-			{
-				// default options are shown. On some platforms
-				// these options are set automatically — see below
-				pages: 'build',
-				assets: 'build',
-				fallback: null,
-				precompress: false
-			}
-		),
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		}
+		adapter: adapter()
 	}
 };
 
